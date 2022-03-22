caad5f74150f7c13d61393186c450059c021a32f
DESKTOP-1CL2VLB\matte <matte_920319@live.se>
2022-03-22 11:04:43 +0100

    Created a skeleton for the pages and installed react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        {/* Todo Navbar */}
+        <h1>My App</h1>
+        <Routes>
+          <Route exact path="/*" element={<Explore />} />
+          <Route exact path="/offers" element={<Offers />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
